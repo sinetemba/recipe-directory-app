88e3daea26b1c20160b84456aaf80199d275e4e1
sinetemba.nkosana@mercedes-benz.com <sinetemba.nkosana@mercedes-benz.com>
2022-08-04 09:19:29 +0200

    pages and router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/home/Home';
+import Create from './pages/create/create';
+import Search from './pages/search/Search';
+import Recipe from './pages/recipe/Recipe';
+
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+       <Switch>
+        
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route  path="/create">
+          <Create/>
+        </Route>
+        <Route  path="/search">
+          <Search/>
+        </Route>
+        <Route path="/recipes/:id">
+          <Recipe/>
+        </Route>
+
+       </Switch>
+      </BrowserRouter>
     </div>
   );
 }
