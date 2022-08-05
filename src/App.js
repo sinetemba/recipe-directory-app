172818caaafd5fbf0555208ef91dddb0024a321a
sinetemba.nkosana@mercedes-benz.com <sinetemba.nkosana@mercedes-benz.com>
2022-08-05 12:15:10 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/home/Home';
-import Create from './pages/create/create';
+import Create from './pages/create/Create';
 import Search from './pages/search/Search';
 import Recipe from './pages/recipe/Recipe';
 
 import './App.css'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+
+      <Navbar></Navbar>
        <Switch>
-        
+
         <Route exact path="/">
           <Home/>
         </Route>
