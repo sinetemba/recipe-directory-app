7ff6bf35ff49944b0b08583bde3ef1ce6b6c6e2e
sinetemba.nkosana@mercedes-benz.com <sinetemba.nkosana@mercedes-benz.com>
2022-08-05 17:29:33 +0200

    recipeList, recipe details and styling

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,19 @@
 
+import { useFetch } from '../../hooks/useFetch'
+//styles
 import './Home.css'
+// components
+import RecipeList from '../../components/RecipeList'
 
 export default function Home() {
+
+  const {data : recipesData, isPending,error} = useFetch(' http://localhost:3000/recipes')
+
   return (
-    <div>Home</div>
+    <div className='home'>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {recipesData && <RecipeList recipes={recipesData}/> }
+    </div>
   )
 }
--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,7 +1,28 @@
+import {useParams, useHistory} from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch';
 import './Recipe.css'
 
 export default function Recipe() {
+
+  const { id } = useParams();
+  const url = 'http://localhost:3000/recipes/' + id;
+  const {data: recipe, isPending, error } = useFetch(url);
+  const history = useHistory();
+
   return (
-    <div>Recipe</div>
+    <div className="recipe">
+    {error && <p className="error">{error}</p>}
+    {isPending && <p className="loading">Loading...</p>}
+    {recipe && (
+      <>
+        <h2 className="page-title">{recipe.title}</h2>
+        <p>Takes {recipe.cookingTime} to cook.</p>
+        <ul>
+          {recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
+        </ul>
+        <p className="method">{recipe.method}</p>
+      </>
+    )}
+  </div>
   )
 }
