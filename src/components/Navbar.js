a1856b033309d27d69936df29b129900d2619db8
sinetemba.nkosana@mercedes-benz.com <sinetemba.nkosana@mercedes-benz.com>
2022-09-02 15:13:15 +0200

    creating context, provider and custom context hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,18 @@
 import { Link } from 'react-router-dom'
-
+import { useTheme } from '../hooks/useTheme'
 //style
 import './Navbar.css'
 
 //components
 import Searchbar from './Searchbar'
 
+
 export default function Navbar() {
+ const {color} = useTheme()
+ 
+
   return (
-    <div className='navbar'>
+    <div className='navbar' style={{background: color}}>
         <nav>
           <Link to="/" className='brand'>
             <h1>Cooking Ninja</h1>
