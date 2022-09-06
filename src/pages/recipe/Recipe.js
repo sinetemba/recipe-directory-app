871602de192760164bf574dcf5feb6f95abac32a
sinetemba.nkosana@mercedes-benz.com <sinetemba.nkosana@mercedes-benz.com>
2022-09-06 21:43:35 +0200

    fetching firestore document

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,29 +1,51 @@
-import {useParams, useHistory} from 'react-router-dom'
-import { useFetch } from '../../hooks/useFetch';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom'
 import { useTheme } from '../../hooks/useTheme';
+import { projectFirestore } from '../../firebase/config'
 import './Recipe.css'
 
 export default function Recipe() {
 
   const { id } = useParams();
-  const url = 'http://localhost:3000/recipes/' + id;
-  const {data: recipe, isPending, error } = useFetch(url);
   const { mode } = useTheme();
+
+  const [recipe, setRecipe] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    setIsPending(true);
+
+    projectFirestore.collection('recipes').doc(id).get().then((doc) => {
+
+      if (doc.exists) {
+        setIsPending(false);
+        setRecipe(doc.data());
+      } else {
+        setIsPending(false);
+        setError('Recipe not found.');
+      }
+
+    })
+
+
+
+  }, [id])
 
   return (
     <div className={`recipe ${mode}`}>
-    {error && <p className="error">{error}</p>}
-    {isPending && <p className="loading">Loading...</p>}
-    {recipe && (
-      <>
-        <h2 className="page-title">{recipe.title}</h2>
-        <p>Takes {recipe.cookingTime} to cook.</p>
-        <ul>
-          {recipe.ingredients.map(ing => <li key={ing}>ing</li>)}
-        </ul>
-        <p className="method">{recipe.method}</p>
-      </>
-    )}
-  </div>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+      )}
+    </div>
   )
 }
