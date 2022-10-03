ba0c12ac3fc6a6735ebeca7ca98113297e552fc9
sinetemba.nkosana@mercedes-benz.com <sinetemba.nkosana@mercedes-benz.com>
2022-10-03 15:30:52 +0200

    removed warning

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,20 +8,20 @@ import Searchbar from './Searchbar'
 
 
 export default function Navbar() {
- const {color} = useTheme()
- 
+  const { color } = useTheme()
+
 
   return (
-    <div className='navbar' style={{background: color}}>
-        <nav>
-          <Link to="/" className='brand'>
-            <h1>Cooking Ninja</h1>
-          </Link>
-          <Searchbar />
-          <Link to="/create">
-            <h1>Create Recipe</h1>
-          </Link>
-        </nav>
+    <div className='navbar' style={{ background: color }}>
+      <nav>
+        <Link to="/" className='brand'>
+          <h1>Cooking Ninja</h1>
+        </Link>
+        <Searchbar />
+        <Link to="/create">
+          <h1>cipe</h1>
+        </Link>
+      </nav>
     </div>
   )
 }
